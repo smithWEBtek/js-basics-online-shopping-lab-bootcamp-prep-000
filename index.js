86ec1c07fb559811fa0aeee6b1fb482a860ff706
smithwebtek <brad@smithwebtek.com>
2018-04-22 09:39:09 -0400

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ function addToCart(item) {
 	return `${item.itemName} has been added to your cart.`
 }
 
-function viewCart() {
-	return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
-}
-
 function total() {
 	var sum = sumUpPrices()
 	return sum
@@ -51,19 +47,23 @@ function generateCartItem(itemName) {
 	}
 }
 
-function generateCartDescription() {
+function viewCart() {
 	var cartDescription = 'In your cart, you have '
-	if (getCart().length >= 1) {
-		cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
-	}
-	if (getCart().length >= 2) {
-		var middleCartItemsDescription = ''
-		for (var i = 1; i < getCart().length - 1; i++) {
-			middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
-		}
-		cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}`
-	}
 
+	if (getCart().length === 0) {
+		return "Your shopping cart is empty."
+	} else {
+		if (getCart().length >= 1) {
+			cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+		}
+		if (getCart().length >= 2) {
+			var middleCartItemsDescription = ''
+			for (var i = 1; i < getCart().length - 1; i++) {
+				middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+			}
+			cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}`
+		}
+	}
 	return `${cartDescription}.`
 }
 
